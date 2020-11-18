061333b596bd41b36bd0f20fa185d774c00ad65b
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-11-18 10:01:44 +0100

    Moved links from index to its own component

--- src/app.js
+++ src/app.js
@@ -114,49 +114,7 @@ getUpcomingLaunches();
 --- <<<     Goals and video links     >>> ---
 ---############################################################ */
 const goalsAndLinks = document.querySelector('.index__goalsAndLinks');
-goalsAndLinks.innerHTML = `<section class="index__goalSection-1 background-image">
-<h3>SpaceX goal is to enable people to live on another planet. Most notably, Mars.</h3>
-</section>
-<section class="index__videoLinksSection">
-<div class="index__videoLinksContainer">
-  <h2>AMBITIONS</h2>
-  <div class="index__videoCardsRow">
-    <div class="index__videoLinkCard">
-      <img src="./assets/content/falcon-heavy-test-flight-small.jpg" alt="Falcon Heavy test flight.">
-      <p>Falcon Heavy | Test fligh</p>
-      <div class="index__videoLinkButton">
-        <a target="_blank" href="https://youtu.be/wbSwFU6tY1c">Watch video</a>
-      </div>
-    </div>
-    <div class="index__videoLinkCard">
-      <img src="./assets/content/spacex-on-mars-small.jpg" alt="SpaceX on Mars.">
-      <p>SpaceX on Mars</p>
-      <div class="index__videoLinkButton">
-        <a target="_blank" href="https://youtu.be/FYU-N2RWfso">Watch video</a>
-      </div>
-    </div>
-  </div>
-  <div class="index__videoCardsRow">
-    <div class="index__videoLinkCard">
-      <img src="./assets/content/starship-journey-to-mars-small.jpg" alt="Starship journey to Mars">
-      <p>Starship | journey to Mars</p>
-      <div class="index__videoLinkButton">
-        <a target="_blank" href="https://youtu.be/-kU6fMGh4l4">Watch video</a>
-      </div>
-    </div>
-    <div class="index__videoLinkCard">
-      <img src="./assets/content/starship-sn6-testflight-small.jpg" alt="Starship SN6 test flight.">
-      <p>Starship | SN6 | test flight</p>
-      <div class="index__videoLinkButton">
-        <a target="_blank" href="https://youtu.be/MdAKrzOLQTg">Watch video</a>
-      </div>
-    </div>
-  </div>
-</div>
-</section>
-<section class="index__goalSection-2 background-image">
-<h3>The best way to accomplish that, is to re-use as much of the rocket as possible.</h3>
-</section>`;
+goalsAndLinks.innerHTML = indexGoalsLinks;
 
 
 /* ############################################################
