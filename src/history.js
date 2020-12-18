1f1ebe0e96d105c7f1173b7e62a84981b9cc5b43
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-18 13:28:37 +0100

    New version of API for history page

--- src/history.js
+++ src/history.js
@@ -1,39 +1,32 @@
-const historyUrl = "https://api.spacexdata.com/v3/history";
+const historyUrl = "https://api.spacexdata.com/v4/history";
 const historyContainer = document.querySelector(".history__container");
 
 const getHistory = async () => {
   try {
     const response = await fetch(historyUrl);
     const historyResults = await response.json();
-    createHistoryHtml(historyResults)
-  }
-  catch(error) {
-    historyContainer.innerHTML = displayError("Ooops, An error occured when calling API.")
-  }
-}
-getHistory()
 
+    createHistoryHtml(historyResults);
+  } catch (error) {
+    historyContainer.innerHTML = displayError(
+      "Ooops, An error occured when calling API."
+    );
+  }
+};
+getHistory();
 
 const createHistoryHtml = (historyResults) => {
-
-  historyResults.map(history => {
-
+  historyResults.map((history) => {
     const title = history.title;
     const eventDate = history.event_date_utc;
     const details = history.details;
     const articleLink = history.links.article;
-    const flightNumber = history.flight_number;
-
-    const flight = () => {
-      return flightNumber === null ? 'N/A' : flightNumber;
-    };
 
     historyContainer.innerHTML += `
     <div class="history__card">
       <div class="history__cardTextContainer">
       <p class="history__date">${americanDateFormat(eventDate)}</p>
         <h2>${title}</h2>
-        <p><b>Flight#:</b> ${flight()}</p>
         <p class="history__description">${details}</p>
         <div class="history__link">
           <a target="_blank" href="${articleLink}">Article</a>
@@ -46,5 +39,5 @@ const createHistoryHtml = (historyResults) => {
 /* ############################################################
 --- <<<     Footer     >>> ---
 ---############################################################ */
-const pagesFooter = document.querySelector('.pages__footer');
+const pagesFooter = document.querySelector(".pages__footer");
 pagesFooter.innerHTML = pagesFooterHtml;
